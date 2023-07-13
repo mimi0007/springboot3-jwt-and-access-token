fc47eda1d310ddc1b7c084a5d1885b19e60f3265
mimi0007 <afsanaaziz.mimi@gmail.com>
2023-07-13 15:36:56 +0600

    Initial commit

---
+++ src/main/java/com/ownlearning/Springboot3JwtAndRefreshTokenApplication.java
@@ -0,0 +1,13 @@
+package com.ownlearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot3JwtAndRefreshTokenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot3JwtAndRefreshTokenApplication.class, args);
+	}
+
+}
