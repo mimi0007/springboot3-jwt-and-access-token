5878e876aaeb6baaa2d3358eb7175283c0019dc8
mimi0007 <afsanaaziz.mimi@gmail.com>
2023-07-13 16:36:46 +0600

    Create User class

---
+++ src/main/java/com/ownlearning/models/User.java
@@ -0,0 +1,23 @@
+package com.ownlearning.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String username;
+    private String password;
+    @ManyToMany()
+    private List<Role> roles;
+}
