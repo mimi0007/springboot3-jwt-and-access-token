04902e4edc9c2c1ed09925367f8cd3d0e515b0fa
mimi0007 <afsanaaziz.mimi@gmail.com>
2023-07-18 16:04:12 +0600

    Refactor Role class to Enum

--- src/main/java/com/ownlearning/models/Role.java
+++ src/main/java/com/ownlearning/models/Role.java
@@ -1,23 +1,9 @@
 package com.ownlearning.models;
 
-import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import jakarta.persistence.Table;
 
-@Entity
-@Data
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor
 @Table(name = "roles")
-public class Role {
-
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    private String email;
+public enum Role {
+    ADMIN,
+    USER
 }
