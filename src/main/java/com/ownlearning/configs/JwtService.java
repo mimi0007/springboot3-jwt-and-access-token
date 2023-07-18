327af5bfdf904ad9e56e25de1ff21699da0dfd3b
mimi0007 <afsanaaziz.mimi@gmail.com>
2023-07-18 12:27:30 +0600

    Create JwtService class

---
+++ src/main/java/com/ownlearning/configs/JwtService.java
@@ -0,0 +1,11 @@
+package com.ownlearning.configs;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class JwtService {
+
+    public String extractUsername (String jwt) {
+        return null;
+    }
+}
