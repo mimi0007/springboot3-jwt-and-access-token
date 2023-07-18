34212e26071b7a1f5d055bc3a81b4242347976fc
mimi0007 <afsanaaziz.mimi@gmail.com>
2023-07-18 16:39:54 +0600

    Add generateToken method

--- src/main/java/com/ownlearning/configs/JwtService.java
+++ src/main/java/com/ownlearning/configs/JwtService.java
@@ -3,22 +3,41 @@ package com.ownlearning.configs;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jws;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
 
 import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class JwtService {
 
-    public static final String SECRET_KEY = "and0cmVmcmVzaHRva2Vu";
+    public static final String SECRET_KEY = "dcd092f32c19b9287f38ffad97fc2b12e4fbe9ee31e0afcb370d35f19cc31b23";
+
+    public String generateToken(String useremail) {
+        Map<String, Object> claims = new HashMap<>();
+        return createToken(claims, useremail);
+    }
+
+    private String createToken(Map<String, Object> claims, String useremail) {
+        return Jwts.builder()
+                .setClaims(claims)
+                .setSubject(useremail)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 15)) //15 min
+                .signWith(getSigninKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
 
     public String extractUsername(String jwtToken) {
         return null;
     }
 
-    private Claims extractAllClaims(String jwtToken) {
+    public Claims extractAllClaims(String jwtToken) {
         Jws<Claims> jwsClaims = Jwts
                 .parserBuilder()
                 .setSigningKey(getSigninKey())
