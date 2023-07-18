9c414677202ed66feea5c828e523ec76e67a5bdd
mimi0007 <afsanaaziz.mimi@gmail.com>
2023-07-18 11:42:48 +0600

    Add annotations

--- src/main/java/com/ownlearning/models/Role.java
+++ src/main/java/com/ownlearning/models/Role.java
@@ -1,17 +1,17 @@
 package com.ownlearning.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Data
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@Table(name = "roles")
 public class Role {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
