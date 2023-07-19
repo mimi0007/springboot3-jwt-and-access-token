0a5a7ac8a336db9f67da79313a8a7bd080651f4d
mimi0007 <afsanaaziz.mimi@gmail.com>
2023-07-19 16:52:31 +0600

    Refactor CustomUserDetailsService class

--- src/main/java/com/ownlearning/configs/CustomUserDetailsService.java
+++ src/main/java/com/ownlearning/configs/CustomUserDetailsService.java
@@ -1,20 +1,24 @@
 package com.ownlearning.configs;
 
 import com.ownlearning.repositories.UserRepository;
-import org.springframework.context.annotation.Bean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.stereotype.Service;
 
-@Service
-public class CustomUserDetailsService {
+@Configuration
+public class CustomUserDetailsService implements UserDetailsService {
 
+    //use autowire in this case as a bean of this service is created in security config and raising a constructor issue
+    @Autowired
     private UserRepository userRepository;
 
     //will help to know whether the user is present in db or not
-    @Bean
-    public UserDetailsService userDetailsService() {
-        return username -> userRepository.findByUserEmail(username)
-                .orElseThrow(() -> new UsernameNotFoundException("User is not found!"));
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        return userRepository.findByUserEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
     }
 }
