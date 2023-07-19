b47f698e6c6ea10759223245fe7134cba4b0b5a3
mimi0007 <afsanaaziz.mimi@gmail.com>
2023-07-19 09:49:19 +0600

    Create AuthController class

---
+++ src/main/java/com/ownlearning/controllers/AuthController.java
@@ -0,0 +1,43 @@
+package com.ownlearning.controllers;
+
+import com.ownlearning.requests.AuthRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/auth")
+public class AuthController {
+
+    private AuthenticationManager authManager;
+
+    @PostMapping("/register")
+    public String register(@RequestBody String useremail) {
+        return "register!";
+    }
+
+    @PostMapping("/login")
+    public String login(@RequestBody AuthRequest authRequest) {
+        Authentication authentication = authManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        authRequest.getUserEmail(),
+                        authRequest.getPassword())
+        );
+
+        if (authentication.isAuthenticated()) {
+            return "login!";
+        } else {
+            throw new UsernameNotFoundException("User is not valid!");
+        }
+
+    }
+
+
+}
