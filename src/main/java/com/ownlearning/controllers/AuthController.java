15550583330d47c231508074eb489dd461dac288
mimi0007 <afsanaaziz.mimi@gmail.com>
2023-07-20 12:45:00 +0600

    Refactor return type

--- src/main/java/com/ownlearning/controllers/AuthController.java
+++ src/main/java/com/ownlearning/controllers/AuthController.java
@@ -1,30 +1,38 @@
 package com.ownlearning.controllers;
 
 import com.ownlearning.requests.AuthRequest;
+import com.ownlearning.requests.RegisterRequest;
+import com.ownlearning.responses.AuthResponse;
+import com.ownlearning.responses.RegisterResponse;
+import com.ownlearning.services.AuthService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequiredArgsConstructor
 @RequestMapping("/auth")
 public class AuthController {
 
     private final AuthenticationManager authManager;
 
+    private final AuthService authService;
+
     @PostMapping("/register")
-    public String register(@RequestBody String useremail) {
-        return "register!";
+    public ResponseEntity<RegisterResponse> register(@RequestBody RegisterRequest registerRequest) {
+
+        return ResponseEntity.ok(authService.register(registerRequest));
     }
 
     @PostMapping("/login")
-    public String login(@RequestBody AuthRequest authRequest) {
+    public ResponseEntity<AuthResponse> login(@RequestBody AuthRequest authRequest) {
         Authentication authentication = authManager.authenticate(
                 new UsernamePasswordAuthenticationToken(
                         authRequest.getUserEmail(),
@@ -32,7 +40,7 @@ public class AuthController {
         );
 
         if (authentication.isAuthenticated()) {
-            return "login!";
+            return ResponseEntity.ok(authService.login(authRequest));
         } else {
             throw new UsernameNotFoundException("User is not valid!");
         }
