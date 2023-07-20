6c6cde9a08286083f193de9575d056a2f45698ac
mimi0007 <afsanaaziz.mimi@gmail.com>
2023-07-20 15:34:55 +0600

    Remove authManager field, refactor login method

--- src/main/java/com/ownlearning/controllers/AuthController.java
+++ src/main/java/com/ownlearning/controllers/AuthController.java
@@ -7,10 +7,6 @@ import com.ownlearning.responses.RegisterResponse;
 import com.ownlearning.services.AuthService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,8 +16,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RequestMapping("/auth")
 public class AuthController {
-
-    private final AuthenticationManager authManager;
 
     private final AuthService authService;
 
@@ -33,18 +27,8 @@ public class AuthController {
 
     @PostMapping("/login")
     public ResponseEntity<AuthResponse> login(@RequestBody AuthRequest authRequest) {
-        Authentication authentication = authManager.authenticate(
-                new UsernamePasswordAuthenticationToken(
-                        authRequest.getUserEmail(),
-                        authRequest.getPassword())
-        );
 
-        if (authentication.isAuthenticated()) {
-            return ResponseEntity.ok(authService.login(authRequest));
-        } else {
-            throw new UsernameNotFoundException("User is not valid!");
-        }
-
+        return ResponseEntity.ok(authService.login(authRequest));
     }
 
 
