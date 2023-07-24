ea592a039776cf25490a6c418087dec9e7bc373e
mimi0007 <afsanaaziz.mimi@gmail.com>
2023-07-24 11:42:50 +0600

    Create RefreshTokenRequest class

---
+++ src/main/java/com/ownlearning/requests/RefreshTokenRequest.java
@@ -0,0 +1,15 @@
+package com.ownlearning.requests;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class RefreshTokenRequest {
+    private String refreshToken;
+
+}
