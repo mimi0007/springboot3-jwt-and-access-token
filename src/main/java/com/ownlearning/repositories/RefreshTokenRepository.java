6ee0fcf1b25d9a4e28ea24b39515a47507ed8963
mimi0007 <afsanaaziz.mimi@gmail.com>
2023-07-24 11:42:38 +0600

    Refactor findTokenByUser method to findByRefreshToken

--- src/main/java/com/ownlearning/repositories/RefreshTokenRepository.java
+++ src/main/java/com/ownlearning/repositories/RefreshTokenRepository.java
@@ -1,11 +1,12 @@
 package com.ownlearning.repositories;
 
 import com.ownlearning.models.RefreshToken;
-import com.ownlearning.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
-    RefreshToken findTokenByUser(User user);
+    Optional<RefreshToken> findByRefreshToken(String refreshToken);
 }
